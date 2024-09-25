3810aff2c98d856a70a92d840127e00f5b8a5805
nawkwoo <sonkw4775@naver.com>
2024-09-25 14:23:04 +0900

    week3 homework

---
+++ myTempalate/myTempalate/src/main/java/com/pironeer/myTempalate/MyTempalateApplication.java
@@ -0,0 +1,13 @@
+package com.pironeer.myTempalate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTempalateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTempalateApplication.class, args);
+	}
+
+}
---
+++ myTempalate/myTempalate/src/test/java/com/pironeer/myTempalate/MyTempalateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pironeer.myTempalate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTempalateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
