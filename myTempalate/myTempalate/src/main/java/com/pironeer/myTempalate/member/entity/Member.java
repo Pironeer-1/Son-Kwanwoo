ace1ddce8549b4f02b8c56776dc41bebf456953c
nawkwoo <sonkw4775@naver.com>
2024-09-26 16:53:16 +0900

    MEMBER Flow

---
+++ myTempalate/myTempalate/src/main/java/com/pironeer/myTempalate/member/entity/Member.java
@@ -0,0 +1,20 @@
+package com.pironeer.myTempalate.member.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+    private String memberId;
+    private String password;
+    private String name;
+
+    @Builder
+    public Member(String memberId, String password, String name) {
+        this.memberId = memberId;
+        this.password = password;
+        this.name = name;
+    }
+}
